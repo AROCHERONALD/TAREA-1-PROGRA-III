32f5e4340724c3b3517c1a2e1688039ca975106e
Ronald Antonio Aroche Santos <raroches1@miumg.edu.gt>
2026-02-13 13:47:34 -0600

    Proyecto Maven tarea 1

---
+++ tarea1/src/main/java/com/tarea/App.java
@@ -0,0 +1,14 @@
+package com.tarea;
+
+public class App {
+    public static void main(String[] args) {
+
+        System.out.println("Hola mundo desde Java");
+
+        int a = 6;
+        int b = 7;
+        int resultado = a * b;
+
+        System.out.println("Resultado: " + resultado);
+    }
+}
